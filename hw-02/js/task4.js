6ed69049272e04709fcce635e2d432794f921516
DatexG <datex.provide.life@gmail.com>
2019-12-02 10:57:04 +0200

    hw-02-04-v2

--- hw-02/js/task4.js
+++ hw-02/js/task4.js
@@ -1,5 +1,18 @@
-"use strict"
+"use strict";
 
-const str = 'this is a strictjb fjdbsafkljbaldhfbjadb jvkdfjbvkj';
-const rez = str <=41?str:str.slice(0,41) + '...';
-console.log(rez);
+function formatString{ rez = formatString <= 41 ? formatString : formatString.slice(0, 41) + "..." }
+
+
+
+console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+// вернется оригинальная строка
+
+console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+// вернется форматированная строка
+
+console.log(formatString('Curabitur ligula sapien.'));
+// вернется оригинальная строка
+
+console.log(
+  formatString('Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.'),
+)
