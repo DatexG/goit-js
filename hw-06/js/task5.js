0bb4bb7529a20e96032dc9877dca7cbeebb3e9a8
DatexG <datex.provide.life@gmail.com>
2019-12-16 17:13:46 +0200

    hw-06-06

---
+++ hw-06/js/task5.js
@@ -0,0 +1,10 @@
+"use strict";
+
+import users from "./users.js";
+
+const getUserWithEmail = (users, email) => {
+    return users.filter(user => user.email === email).map(user => user.name)
+  };
+  
+  console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // {объект пользователя Sheree Anthony}
+  console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {объект пользователя Elma Head}
