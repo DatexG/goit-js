409e15fd5e88fdd3da683d5cbed929c10a929e1c
DatexG <datex.provide.life@gmail.com>
2019-12-24 18:00:59 +0200

    top secret 1

--- top-secret/js/task1.js
+++ top-secret/js/task1.js
@@ -3,14 +3,17 @@
 const clickTheButton = document.querySelector('#js-text')
 const image = document.querySelector('.imagess')
 const text = document.querySelector('p')
+const background = document.querySelector('.background')
 
 function  clickButton(){
   image.style.opacity = '1';
   text.style.visibility = "visible";
+  background.style.opacity = '1'
   setTimeout(refresh, 5000);
 }
 
 function refresh(){
+  background.style.opacity ='0'
   image.style.opacity = '0';
   text.style.visibility = "hidden";
 }
